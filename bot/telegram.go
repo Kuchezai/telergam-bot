466ef2b36201f74ef544a5d2775d8c087c06b939
Kuchezai <Kozhenov.A.A@yandex.ru>
2023-05-09 16:32:32 +0300

    feat: create telegram bot

---
+++ bot/telegram.go
@@ -0,0 +1,91 @@
+package bot
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"telegram-bot/entity"
+)
+
+const (
+	getUpdatesEndpoint  = "/getUpdates"
+	sendMessageEndpoint = "/sendMessage"
+)
+
+type TelegramBot struct {
+	apiURL string
+	token  string
+	router *Router
+}
+
+func New(apiURL, token string, router *Router) *TelegramBot {
+	return &TelegramBot{apiURL, token, router}
+}
+
+func (b *TelegramBot) AskAndServe() {
+	offset := 0
+	for {
+		updates, err := b.getUpdates(offset)
+		if err != nil {
+			log.Printf("Error getting updates: %v", err)
+			continue
+		}
+		for _, update := range updates {
+			b.sendMessage(b.router.HandleUpdate(update.Message))
+			offset = update.UpdateID + 1
+		}
+
+	}
+}
+
+func (b *TelegramBot) getUpdates(offset int) ([]update, error) {
+
+	u := b.apiURL + b.token + getUpdatesEndpoint
+	query := "?offset=" + strconv.Itoa(offset)
+
+	resp, err := http.Get(u + query)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result getUpdatesResult
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, err
+	}
+
+	return result.Updates, nil
+}
+
+func (b *TelegramBot) sendMessage(msg entity.Response) {
+	body, err := json.Marshal(msg)
+	if err != nil {
+		log.Printf("Error marshaling message: %v", err)
+		return
+	}
+	u := b.apiURL + b.token + sendMessageEndpoint
+	resp, err := http.Post(u, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		log.Printf("Error marshaling message: %v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+}
+
+type update struct {
+	UpdateID int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
+
+type getUpdatesResult struct {
+	Updates []update `json:"result"`
+}
